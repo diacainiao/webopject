b3e186331b662fb6672deb5de4690a7df2d2a42d
diacainiao@126.com <diacainiao@126.com@11137926-5862-ff41-ea7c-8d918497c8c8>
2012-06-10 03:54:22 +0000

    初始导入。

---
+++ trunk/boya/zsjz.php
@@ -0,0 +1,9 @@
+<?php
+include_once("action/smarty_inc.php");
+include_once("action/lang.class.php");
+include_once("action/mysql.class.php");
+include_once("action/public_info.php");
+
+
+$smarty->display("zsjs.html");
+?>
